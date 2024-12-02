ad9417e2cf95f61617136ab8fbf44072c54943b3
dhanushrajgp <gdhanush68@gmail.com>
2024-12-02 12:08:51 +0530

    fix/ the error with deserialize in the type field of credentialSubject and StatusVerifiableResult

--- vc-status-verifier/src/main/java/com/credenceid/vcstatusverifier/entity/CredentialSubject.java
+++ vc-status-verifier/src/main/java/com/credenceid/vcstatusverifier/entity/CredentialSubject.java
@@ -1,13 +1,18 @@
 package com.credenceid.vcstatusverifier.entity;
 
+import com.credenceid.vcstatusverifier.util.TypeDeserializer;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class CredentialSubject {
     private String id;
-    private String type;
+    @JsonDeserialize(using = TypeDeserializer.class)
+    private List<String> type;
     private String statusPurpose;
     private String encodedList;
 }
