e31308a2c4cfef7df2e714f5efa360241cda43b5
dhanushrajgp <gdhanush68@gmail.com>
2024-12-10 12:25:18 +0530

    update/vc-status-library - completed all the test cases on utils for 100% codecoverage

--- vc-status-verifier/src/test/java/com/credenceid/vcstatusverifier/util/UtilsTest.java
+++ vc-status-verifier/src/test/java/com/credenceid/vcstatusverifier/util/UtilsTest.java
@@ -1,5 +1,6 @@
 package com.credenceid.vcstatusverifier.util;
 
+import com.credenceid.vcstatusverifier.exception.ServerException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,11 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
     @Test
-    void testDecodeStatusList_validEncodedString() throws IOException {
+    void testDecodeStatusList_validEncodedString_ResultTrue() throws IOException {
         String encoded = "uH4sIAAAAAAAAA-3OMQEAAAgDoEU3ugEWwENIQMI3cx0AAAAAAAAAAAAAAAAAAACgLGiNcIEAQAAA"; // Base64 encoding of "StatusListTest"
         boolean result = Utils.decodeStatusList(encoded, 4000, 1);
         System.out.println("This is the result: " + result);
         assertTrue(result, "The decoded string should match the expected result");
+    }
+
+    @Test
+    void testDecodeStatusList_validEncodedString_ResultFalse() throws IOException {
+        String encoded = "uH4sIAAAAAAAAA-3OMQEAAAgDoEU3ugEWwENIQMI3cx0AAAAAAAAAAAAAAAAAAACgLGiNcIEAQAAA"; // Base64 encoding of "StatusListTest"
+        boolean result = Utils.decodeStatusList(encoded, 4001, 1);
+        System.out.println("This is the result: " + result);
+        assertFalse(result, "The decoded string should match the expected result");
     }
 
     @Test
@@ -32,9 +41,26 @@ class UtilsTest {
     }
 
     @Test
-    void testDecodeStatusList_invalidBase64String() {
+    void testDecodeStatusList_invalidBase64StringNotStartsWith_u() {
         String invalidEncoded = "NotBase64!";
-        assertThrows(IllegalArgumentException.class, () ->
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
                 Utils.decodeStatusList(invalidEncoded, 12, 1));
+        assertEquals("encoded list must start with 'u' ", exception.getMessage());
+    }
+
+    @Test
+    void testDecodeStatusList_invalidBase64String() {
+        String invalidEncoded = "uNotBase64!";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Utils.decodeStatusList(invalidEncoded, 12, 1));
+        assertEquals("The provided string is not a valid Base64URL-encoded string", exception.getMessage());
+    }
+
+    @Test
+    void testGetBitAtIndex_RANGE_ERROR() {
+        String invalidEncoded = "H4sIAAAAAAAAA-3OMQEAAAgDoEU3ugEWwENIQMI3cx0AAAAAAAAAAAAAAAAAAACgLGiNcIEAQAAA";
+        Exception exception = assertThrows(ServerException.class, () ->
+                Utils.getBitAtIndex(invalidEncoded, 100000, 1));
+        assertEquals(Constants.RANGE_ERROR, exception.getMessage());
     }
 }
